25b2123a1a6d5c985c0990aab77ed8e9fb759cbf
Thomas Bodart <thomas.bodart@student-cs.fr>
2025-04-08 10:45:16 +0200

    get graphs with node features

--- graph_analysis/get_graphs.py
+++ graph_analysis/get_graphs.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pathlib import Path
 
@@ -16,6 +17,8 @@ assert os.path.isdir(
 
 lastfm_edges_path = lastfm_data_path / "lastfm_asia_edges.csv"
 deezer_edges_path = deezer_data_path / "deezer_europe_edges.csv"
+lastfm_nattr_path = lastfm_data_path / "lastfm_asia_features.json"
+deezer_nattr_path = deezer_data_path / "deezer_europe_features.json"
 
 assert os.path.exists(
     deezer_edges_path
@@ -38,5 +41,17 @@ def read_edges(file_path):
 lastfm_edges = read_edges(lastfm_edges_path)
 deezer_edges = read_edges(deezer_edges_path)
 
-deezer_graph = nx.from_edgelist(deezer_edges)
-lastfm_graph = nx.from_edgelist(lastfm_edges)
+deezer_graph: nx.graph.Graph = nx.from_edgelist(deezer_edges)
+lastfm_graph: nx.graph.Graph = nx.from_edgelist(lastfm_edges)
+
+with open(deezer_nattr_path, "r") as f:
+    deezer_node_attributes = json.load(f)
+
+with open(lastfm_nattr_path, "r") as f:
+    lastfm_node_attributes = json.load(f)
+
+for node_idx, node_features in deezer_node_attributes.items():
+    deezer_graph.nodes[int(node_idx)]["feat"] = node_features
+
+for node_idx, node_features in lastfm_node_attributes.items():
+    lastfm_graph.nodes[int(node_idx)]["feat"] = node_features
