ccc2ab3d522e4d9841f9e98f0712c35f96bfd730
Thomas Bodart <thomas.bodart@student-cs.fr>
2025-04-14 10:18:39 +0200

    One hot encoding

--- graph_analysis/get_graphs.py
+++ graph_analysis/get_graphs.py
@@ -19,6 +19,8 @@ lastfm_edges_path = lastfm_data_path / "lastfm_asia_edges.csv"
 deezer_edges_path = deezer_data_path / "deezer_europe_edges.csv"
 lastfm_nattr_path = lastfm_data_path / "lastfm_asia_features.json"
 deezer_nattr_path = deezer_data_path / "deezer_europe_features.json"
+lastfm_targt_path = lastfm_data_path / "lastfm_asia_target.csv"
+deezer_targt_path = deezer_data_path / "deezer_europe_target.csv"
 
 assert os.path.exists(
     deezer_edges_path
@@ -26,8 +28,21 @@ assert os.path.exists(
 assert os.path.exists(
     lastfm_edges_path
 ), f"LastFM edges file is missing: {lastfm_edges_path}"
+assert os.path.exists(
+    deezer_nattr_path
+), f"Deezer node attributes file is missing: {deezer_nattr_path}"
+assert os.path.exists(
+    lastfm_nattr_path
+), f"LastFM node attributes file is missing: {lastfm_nattr_path}"
+assert os.path.exists(
+    deezer_targt_path
+), f"Deezer target file is missing: {deezer_targt_path}"
+assert os.path.exists(
+    lastfm_targt_path
+), f"LastFM target file is missing: {lastfm_targt_path}"
 
 
+### Create edges
 def read_edges(file_path):
     edges = []
     with open(file_path, "r") as file:
@@ -44,14 +59,55 @@ deezer_edges = read_edges(deezer_edges_path)
 deezer_graph: nx.graph.Graph = nx.from_edgelist(deezer_edges)
 lastfm_graph: nx.graph.Graph = nx.from_edgelist(lastfm_edges)
 
+### Node features
+
 with open(deezer_nattr_path, "r") as f:
     deezer_node_attributes = json.load(f)
 
 with open(lastfm_nattr_path, "r") as f:
     lastfm_node_attributes = json.load(f)
 
+# Get max of values of edge features
+max_deezer_features = max(
+    [max(features) for features in deezer_node_attributes.values() if len(features) > 0]
+)
+max_lastfm_features = max(
+    [max(features) for features in lastfm_node_attributes.values() if len(features) > 0]
+)
+# print(max_deezer_features)
+# print(max_lastfm_features)
+
 for node_idx, node_features in deezer_node_attributes.items():
-    deezer_graph.nodes[int(node_idx)]["feat"] = node_features
+    one_hot_encoded = [0] * (max_deezer_features + 1)
+    for feature in node_features:
+        one_hot_encoded[feature] = 1
+    deezer_graph.nodes[int(node_idx)]["feat"] = one_hot_encoded
 
 for node_idx, node_features in lastfm_node_attributes.items():
-    lastfm_graph.nodes[int(node_idx)]["feat"] = node_features
+    one_hot_encoded = [0] * (max_lastfm_features + 1)
+    for feature in node_features:
+        one_hot_encoded[feature] = 1
+    lastfm_graph.nodes[int(node_idx)]["feat"] = one_hot_encoded
+
+
+### Target
+def read_targets(file_path):
+    targets = {}
+    with open(file_path, "r") as file:
+        next(file)
+        for line in file:
+            node, target = line.strip().split(",")
+            targets[int(node)] = int(target)
+    return targets
+
+
+deezer_targets = read_targets(deezer_targt_path)
+lastfm_targets = read_targets(lastfm_targt_path)
+
+for node_idx, target in deezer_targets.items():
+    deezer_graph.nodes[node_idx]["target"] = target
+
+for node_idx, target in lastfm_targets.items():
+    lastfm_graph.nodes[node_idx]["target"] = target
+
+print(deezer_graph.nodes[0])
